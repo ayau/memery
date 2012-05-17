e3fea7d1a67c891038007a6f8ca0c299be0dac46
Alex Yau <ayau@sas.upenn.edu>
2012-05-17 14:29:43 +0800

    prototype dynamic meme creator

--- website/index.php
+++ website/index.php
@@ -12,3 +12,17 @@
 <center><img src="meme_creator.php?meme=idontalways&top_text=i don't always make meme generators&bottom_text=but when I do, I make sure sentences longer than one line can fit" alt="I don't always fail to display this meme. But when I do, I display this text instead." /></center>
 
 <center><img src="meme_creator.php?meme=greg&top_text=creates a new popular meme generator&bottom_text=automatically adjusts the font size of longer texts to make sure they all fit" alt="I don't always fail to display this meme. But when I do, I display this text instead." /></center>
+
+<center id='successkid'><img src="meme_creator.php?meme=successkid&top_text=%20&bottom_text=%20" alt="I don't always fail to display this meme. But when I do, I display this text instead." /></center>
+<center><input id='toptext' type="text" size='50' placeholder='Top Text' value="Tried to make the generator respond to input"/></center>
+<center><input id='bottomtext' type="text" size='50' placeholder="Bottom Text" value="Success"/></center>
+<center><input id='submit' type="button" value='submit'/></center>
+
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript">
+		$("#submit").live("click",function(){
+			$("#successkid").empty();
+			$("#successkid").append("<img src='meme_creator.php?meme=successkid&top_text="+$("#toptext").val()+"&bottom_text="+$("#bottomtext").val()+"' alt=\"I don't always fail to display this meme. But when I do, I display this text instead.\" />");
+		})
+	
+	</script>
