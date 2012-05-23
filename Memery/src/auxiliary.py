bb3bada6be56395088549ce9ad3df9b69574d562
Josh Bolgar <jbolgar@sas.upenn.edu>
2012-05-22 20:00:11 -0400

    Updated get_datetime() in axuiliary.py

--- Memery/src/auxiliary.py
+++ Memery/src/auxiliary.py
@@ -4,5 +4,7 @@ def remove_commas(s):
     split = s.split(",")
     return "".join(split)
 
-def get_time():
+def get_datetime():
+    """Returns the current date and time"""
+    #this function is intended to standardize timestamps throughout the project
     return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
