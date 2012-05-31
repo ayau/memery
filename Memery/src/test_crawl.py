2f47df5188c1a991ff9fe0fd783c9fb4ff9aaae8
Josh Bolgar <jbolgar@sas.upenn.edu>
2012-05-31 12:18:31 -0400

    Classified 40 memes in 31 minutes.  Used classify_helper.py to automatically write the file names for me.

--- Memery/src/test_crawl.py
+++ Memery/src/test_crawl.py
@@ -12,19 +12,13 @@ crawler = QM_Crawler(sort_by)
 #out_folder = "Crawl of " + sort_by + " at " + now
 
 def run_test_crawl(crawler_iterator):
-    try:
-        for caption in crawler_iterator:
-            print caption.get_qm_id(), "--", caption.meme.meme_name
-            image_source = caption.image_source
-            print "url:", caption.get_url()
-            print "image_source:", image_source 
-            print DatabaseConnection.insertCrawlData(caption.get_url(),caption.get_filename(), caption.get_rating(), caption.get_views())
-    except HTTPError as e:
-        print """HTTPError \n ** \n ** \n ** \n ** \n ** \n ** \n ** \n ** \n ** \n ** 
-
-retrying."""
-        run_test_crawl(crawler_iterator)
-    
+    for caption in crawler_iterator:
+        print caption.get_qm_id(), "--", caption.meme.meme_name
+        image_source = caption.image_source
+        print "url:", caption.get_url()
+        print "image_source:", image_source 
+        print DatabaseConnection.insertCrawlData(caption.get_url(),caption.get_filename(), caption.get_rating(), caption.get_views())
+    run_test_crawl(crawler_iterator)   
 run_test_crawl(crawler.crawl(30,35))
 
 print "DONE"
